7af8f346e8d1ad5eecc5c3405502fde21b711039
Martin Gütlein <guetlein@posteo.de>
2017-01-05 11:59:52 +0100

    update deploy script

--- bin/deploy_to_mogon.sh
+++ bin/deploy_to_mogon.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
+#VERSION="0.0.1-SNAPSHOT"
+VERSION="1.2.0-DEEP"
+
 set -e
 
 cd ../JavaLib
 mvn clean install
 cd -
 
-cd ../WEKALib
+cd ../weka-lib
 mvn clean install
 cd -
 
-cd ../CDKLib
+cd ../cdk-lib
 mvn clean install
 cd -
 
 mvn clean install
 
-cp /home/martin/.m2/repository/org/kramerlab/cfpminer/CFPMiner/0.0.1-SNAPSHOT/CFPMiner-0.0.1-SNAPSHOT-jar-with-dependencies.jar cfpminer.jar
+cp /home/martin/.m2/repository/org/kramerlab/cfpminer/cfp-miner/$VERSION/cfp-miner-$VERSION-jar-with-dependencies.jar cfpminer.jar
 
 
 
-scp cfpminer.jar guetlein@mrbungle.zdv.uni-mainz.de:/home/guetlein/cfpminer/
-ssh guetlein@mrbungle.zdv.uni-mainz.de 'scp cfpminer/cfpminer.jar mguetlei@mogon.zdv.uni-mainz.de:/home/mguetlei/cfpminer/'
+#scp cfpminer.jar guetlein@mrbungle.zdv.uni-mainz.de:/home/guetlein/cfpminer/
+#ssh guetlein@mrbungle.zdv.uni-mainz.de 'scp cfpminer/cfpminer.jar mguetlei@mogon.zdv.uni-mainz.de:/home/mguetlei/cfpminer/'
 
-#scp cfpminer.jar mguetlei@mogon.zdv.uni-mainz.de:/home/mguetlei/cfpminer/
+scp cfpminer.jar mguetlei@mogon.zdv.uni-mainz.de:/home/mguetlei/cfpminer/
 
 #scp add_runner.sh guetlein@mrbungle.zdv.uni-mainz.de:/home/guetlein/cfpminer/; ssh guetlein@mrbungle.zdv.uni-mainz.de 'scp cfpminer/add_runner.sh mguetlei@mogon.zdv.uni-mainz.de:/home/mguetlei/cfpminer/'
 
